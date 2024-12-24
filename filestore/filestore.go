02140e1b0dc47393228d5866c9bf6d727e2b5d3c
Enrico Weigelt, metux IT consult <info@metux.net>
2024-12-24 14:28:27 +0100

    1

--- filestore/filestore.go
+++ filestore/filestore.go
@@ -2,7 +2,6 @@ package filestore
 
 import (
 	"fmt"
-	"github.com/metux/go-nebulon/util"
 	"github.com/metux/go-nebulon/base"
 	"github.com/metux/go-nebulon/wire"
 	"io"
@@ -98,43 +97,6 @@ func (fs FileStore) StoreFile(r io.Reader, headers map[string]string) (wire.Bloc
 	}
 
 	return fs.StoreBlockList(oids)
-}
-
-type FileReader struct {
-	util.ChainedReader
-	fs FileStore
-}
-
-// FIXME: need to support blocklist refs
-func (reader * FileReader) AddRef(ref wire.BlockRef) error {
-	data, err := reader.fs.LoadBlock(ref)
-	if err != nil {
-		log.Printf("readerAddRef: failed reading block: %s\n", err)
-		return err
-	}
-
-	switch ref.Type {
-		case wire.RefType_Blob:
-			reader.AddBytes(data)
-		case wire.RefType_RefList:
-			log.Printf("readerAddRef: unsupported reflist\n", ref.Type)
-
-			bl, err := reader.fs.LoadBlockList(ref)
-			if err != nil {
-				log.Printf("readerAddFile: failed reading block list %s\n", err)
-				return err
-			}
-			log.Printf("BLOCK REF LIST %+v\n", bl.Dump())
-
-			for idx, walk := range bl.Refs {
-				log.Printf("block ref ent %d -- %s\n", idx, walk.Dump())
-				reader.AddRef(*walk)
-			}
-		default:
-			log.Printf("readerAddRef: unsupported ref type %+v (default)\n", ref.Type)
-	}
-
-	return nil
 }
 
 func (fs FileStore) ReadFile(ref wire.BlockRef) (io.Reader, map[string]string, error) {
