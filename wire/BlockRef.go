1c2a4bd22cf425a2d24fa118d2a1a26cc9655764
Enrico Weigelt, metux IT consult <info@metux.net>
2024-12-24 14:20:03 +0100

    1

--- wire/BlockRef.go
+++ wire/BlockRef.go
@@ -9,16 +9,16 @@ func RefForBlock(data []byte) BlockRef {
 	d := sha256.Sum256(data)
 	return BlockRef{
 		Type: RefType_Blob,
-		Key: d[:],
+		Oid: d[:],
 	}
 }
 
 func (ref BlockRef) HexKey() string {
-	return fmt.Sprintf("%s-%X", ref.Type, ref.Key)
+	return fmt.Sprintf("%s-%X", ref.Type, ref.Oid)
 }
 
 func (ref BlockRef) OID() string {
-	return fmt.Sprintf("%X", ref.Key)
+	return fmt.Sprintf("%X", ref.Oid)
 }
 
 func (ref BlockRef) Dump() string {
