f64a45d800c6cf8b97c0bf61edd1bcbb696d8d35
Enrico Weigelt, metux IT consult <info@metux.net>
2024-12-27 15:50:53 +0100

    1

--- blockcrypt/blockcrypt.go
+++ blockcrypt/blockcrypt.go
@@ -16,3 +16,15 @@ func BlockDecrypt(cipher wire.CipherType, key [] byte, data [] byte) ([]byte, er
 			return []byte{}, fmt.Errorf("unsupported cipher type: %s\n", cipher)
 	}
 }
+
+func BlockEncrypt(cipher wire.CipherType, key [] byte, data [] byte) ([]byte, []byte, error) {
+	switch (cipher) {
+		case wire.CipherType_None:
+			return []byte{}, data, nil
+		case wire.CipherType_AES_CBC:
+			encrypted, key, err := AESEncryptBlock(data)
+			return key, encrypted, err
+		default:
+			return []byte{}, []byte{}, fmt.Errorf("storeDataBlock(): unsupported cipher %s\n", cipher)
+	}
+}
