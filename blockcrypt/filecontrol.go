13a02e19bbaaaca95d2410d7b35617f183fbf939
Enrico Weigelt, metux IT consult <info@metux.net>
2024-12-30 15:21:49 +0100

    1

--- blockcrypt/filecontrol.go
+++ blockcrypt/filecontrol.go
@@ -25,3 +25,15 @@ func EncryptFileControl(content_ref wire.BlockRef, headers map[string]string, ci
 
 	return key, encrypted, nil
 }
+
+func DecryptFileControl(cipher wire.CipherType, key []byte, private []byte) (wire.FileControl, error) {
+	fctrl_bin, err := BlockDecrypt(cipher, key, private)
+	if err != nil {
+		return wire.FileControl{}, fmt.Errorf("error decrypting FileControl [%w]", err)
+	}
+
+	fctrl := wire.FileControl{}
+	fctrl.Unmarshal(fctrl_bin)
+
+	return fctrl, nil
+}
