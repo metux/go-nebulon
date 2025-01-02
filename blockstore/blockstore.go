b2677068012b5d8884d3d175684e40eb7242bdee
Enrico Weigelt, metux IT consult <info@metux.net>
2025-01-02 14:55:14 +0100

    1

--- blockstore/blockstore.go
+++ blockstore/blockstore.go
@@ -2,10 +2,15 @@ package blockstore
 
 import (
 	"os"
+	"log"
 	"path/filepath"
 
 	"github.com/metux/go-nebulon/base"
 	"github.com/metux/go-nebulon/wire"
+)
+
+var (
+	TraceWrite = false
 )
 
 type SimpleStore struct {
@@ -26,6 +31,15 @@ func (s SimpleStore) StoreBlock(data []byte) (wire.BlockRef, error) {
 	ref := wire.RefForBlock(data)
 	fn := s.Ref2FN(ref)
 	os.MkdirAll(filepath.Dir(fn), os.ModePerm)
+
+	// dont write already existing objects
+	// 2do: we could check equality here
+	if _, err := os.Stat(fn); err != nil {
+		if TraceWrite {
+			log.Printf("object already exists %s\n", fn)
+		}
+		return ref, nil
+	}
 	err := os.WriteFile(fn, data, 0644)
 	return ref, err
 }
