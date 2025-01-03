3909b835d1c989c881bc74c850755cf2c7abc0e3
Enrico Weigelt, metux IT consult <info@metux.net>
2025-01-03 15:33:24 +0100

    1

--- filestore/filereader.go
+++ filestore/filereader.go
@@ -1,7 +1,6 @@
 package filestore
 
 import (
-	"fmt"
 	"io"
 
 	"github.com/metux/go-nebulon/util"
@@ -11,25 +10,6 @@ import (
 type fileReader struct {
 	util.ChainedReader
 	readerBase
-}
-
-func (reader *fileReader) AddRef(ref wire.BlockRef) error {
-	switch ref.Type {
-	case wire.RefType_Blob:
-		reader.AddReader(NewBlobReader(reader.BlockStore, ref))
-	case wire.RefType_RefList:
-		bl, err := reader.loadBlockList(ref)
-		if err != nil {
-			return err
-		}
-		for _, walk := range bl.Refs {
-			reader.AddRef(*walk)
-		}
-	default:
-		return fmt.Errorf("unsupported ref type %+v\n", ref.Type)
-	}
-
-	return nil
 }
 
 func (r *fileReader) ReadStream(ref wire.BlockRef) (io.ReadCloser, wire.Header, error) {
